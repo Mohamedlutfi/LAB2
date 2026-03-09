1d2fbb1a9d3dd0cec8337cf59129861ec35c2157
Mohamed <Mohamelotfii@gmail.com>
2026-03-09 01:05:29 +0100

    Initial Commit

---
+++ LAB2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LAB2
+//
+//  Created by Mohamed Lutfi Mohamed on 2026-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LAB2/LAB2App.swift
@@ -0,0 +1,17 @@
+//
+//  LAB2App.swift
+//  LAB2
+//
+//  Created by Mohamed Lutfi Mohamed on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct LAB2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
